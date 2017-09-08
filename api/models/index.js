863ce9d2c0c3b8f509b071c0c55ee9391a4c0b80
Abhyudit Jain <abhyudit@binbill.com>
2017-09-08 16:34:11 +0530

    fcm_details table and logout api

--- api/models/index.js
+++ api/models/index.js
@@ -25,7 +25,7 @@ const db = {};
 
 fs.readdirSync(__dirname).filter(file => (file.indexOf('.') !== 0) && (file !== 'index.js')).forEach((file) => {
   const model = sequelize.import(path.join(__dirname, file));
-  model.removeAttribute('id');
+  // model.removeAttribute('id');
   db[model.name] = model;
 });
 
