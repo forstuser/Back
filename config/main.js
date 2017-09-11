6d67329249b789f0b44945e1d5f0539b22ac6121
Abhyudit Jain <abhyudit@binbill.com>
2017-09-11 16:20:45 +0530

    email validation removed

--- config/main.js
+++ config/main.js
@@ -5,40 +5,40 @@
 const fs = require('fs');
 
 module.exports = {
-    SMS: {
-        AUTH_KEY: process.env.SMS_AUTH_KEY,
-    },
-    AWS: {
-        S3: {
-            BUCKET: process.env.AWS_S3_BUCKET,
-            USER_IMAGE: "userimages",
-            CATEGORY_IMAGE: "categoryimages"
-        },
-        ACCESS_DETAILS: {
-            accessKeyId: process.env.AWS_ACCESS_KEY,
-            secretAccessKey: process.env.AWS_SECRET_KEY,
-            region: process.env.AWS_REGION
-        }
-    },
-    SERVER_HOST: process.env.SERVER_HOST,
-    EMAIL: {
-        USER: process.env.EMAIL_ID,
-        PASSWORD: process.env.EMAIL_PASSWORD
-    },
-    // Secret key for JWT signing and encryption
-    JWT_SECRET: fs.readFileSync(`${__dirname}/rsa-private.pem`),
-    // Database connection information
-    DATABASE: {
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_DATABASE,
-        autoReconnect: true,
-        host: process.env.DATABASE_HOST,
-        dialect: 'mysql',
-        port: 3306
-    },
-    APP: {
-        PORT: process.env.APP_PORT || 3000
-    },
-    TRUECALLER_PUBLIC_KEY: '-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDEpFwIarbm48m6ueG+jhpt2vCGaqXZlwR/HPuL4zH1DQ/eWFbgQtVnrta8QhQz3ywLnbX6s7aecxUzzNJsTtS8VxKAYll4E1lJUqrNdWt8CU+TaUQuFm8vzLoPiYKEXl4bX5rzMQUMqA228gWuYmRFQnpduQTgnYIMO8XVUQXl5wIDAQAB\n-----END PUBLIC KEY-----'
+	SMS: {
+		AUTH_KEY: process.env.SMS_AUTH_KEY
+	},
+	AWS: {
+		S3: {
+			BUCKET: process.env.AWS_S3_BUCKET,
+			USER_IMAGE: 'userimages',
+			CATEGORY_IMAGE: 'categoryimages'
+		},
+		ACCESS_DETAILS: {
+			accessKeyId: process.env.AWS_ACCESS_KEY,
+			secretAccessKey: process.env.AWS_SECRET_KEY,
+			region: process.env.AWS_REGION
+		}
+	},
+	SERVER_HOST: process.env.SERVER_HOST,
+	EMAIL: {
+		USER: process.env.EMAIL_ID,
+		PASSWORD: process.env.EMAIL_PASSWORD
+	},
+	// Secret key for JWT signing and encryption
+	JWT_SECRET: fs.readFileSync(`${__dirname}/rsa-private.pem`),
+	// Database connection information
+	DATABASE: {
+		username: process.env.DATABASE_USERNAME,
+		password: process.env.DATABASE_PASSWORD,
+		database: process.env.DATABASE_DATABASE,
+		autoReconnect: true,
+		host: process.env.DATABASE_HOST,
+		dialect: 'mysql',
+		port: 3306
+	},
+	APP: {
+		PORT: process.env.APP_PORT || 3000
+	},
+	TRUECALLER_PUBLIC_KEY: '-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDEpFwIarbm48m6ueG+jhpt2vCGaqXZlwR/HPuL4zH1DQ/eWFbgQtVnrta8QhQz3ywLnbX6s7aecxUzzNJsTtS8VxKAYll4E1lJUqrNdWt8CU+TaUQuFm8vzLoPiYKEXl4bX5rzMQUMqA228gWuYmRFQnpduQTgnYIMO8XVUQXl5wIDAQAB\n-----END PUBLIC KEY-----'
 };
