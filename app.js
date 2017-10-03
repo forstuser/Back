7dd6fbe8ce8c83e84b34c9184f8772b9c4c6fb89
Abhyudit Jain <abhyudit@binbill.com>
2017-10-03 14:41:36 +0530

    minor fix

--- app.js
+++ app.js
@@ -33,7 +33,9 @@ if (process.env.NODE_ENV !== "production") {
 	SERVER_OPTIONS.tls = TLS_OPTIONS;
 }
 
-// server.connection(SERVER_OPTIONS);
+if (process.env.NODE_ENV !== 'production') {
+	server.connection(SERVER_OPTIONS);
+}
 
 const goodLoggingOption = {
 	ops: {
