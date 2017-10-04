d721ef404410f96f1648c80c46d29986fa3b4bf1
Abhyudit Jain <abhyudit@binbill.com>
2017-10-04 14:44:56 +0530

    Comments field size from 255 to 2000 in table_consumer_bills

--- models/bills.js
+++ models/bills.js
@@ -3,40 +3,43 @@
  */
 
 module.exports = (sequelize, Sequelize) => sequelize.define('consumerBills', {
-  bill_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  bill_reference_id: {
-    type: Sequelize.STRING
-  },
-  created_on: {
-    type: Sequelize.DATE(6),
-    defaultValue: Sequelize.NOW
-  },
-  updated_on: {
-    type: Sequelize.DATE(6),
-    defaultValue: Sequelize.NOW
-  },
-  user_id: {
-    type: Sequelize.INTEGER
-  },
-  updated_by_user_id: {
-    type: Sequelize.INTEGER
-  },
-  uploaded_by: {
-    type: Sequelize.INTEGER
-  },
-  user_status: {
-    type: Sequelize.INTEGER
-  },
-  admin_status: {
-    type: Sequelize.INTEGER
-  }
+	bill_id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true
+	},
+	bill_reference_id: {
+		type: Sequelize.STRING
+	},
+	created_on: {
+		type: Sequelize.DATE(6),
+		defaultValue: Sequelize.NOW
+	},
+	updated_on: {
+		type: Sequelize.DATE(6),
+		defaultValue: Sequelize.NOW
+	},
+	user_id: {
+		type: Sequelize.INTEGER
+	},
+	updated_by_user_id: {
+		type: Sequelize.INTEGER
+	},
+	uploaded_by: {
+		type: Sequelize.INTEGER
+	},
+	user_status: {
+		type: Sequelize.INTEGER
+	},
+	admin_status: {
+		type: Sequelize.INTEGER
+	},
+	comments: {
+		type: Sequelize.STRING(2000)
+	}
 }, {
-  freezeTableName: true,
-  defaultPrimaryKey: false,
-  timestamps: false,
-  tableName: 'table_consumer_bills'
+	freezeTableName: true,
+	defaultPrimaryKey: false,
+	timestamps: false,
+	tableName: 'table_consumer_bills'
 });
