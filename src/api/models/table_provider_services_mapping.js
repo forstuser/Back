c1580d0fbf265a50009e20595fa711c2b505cb0d
varunBB <varun@binbill.com>
2018-05-03 15:29:45 +0530

    mdoified the provider services mapping table. added field affiliated_service_id
    Signed-off-by: varunBB <varun@binbill.com>

--- src/api/models/table_provider_services_mapping.js
+++ src/api/models/table_provider_services_mapping.js
@@ -1,81 +1,89 @@
 'use strict';
 
 export default (sequelize, DataTypes) => {
-    const table_provider_services_mapping = sequelize.define('table_provider_services_mapping', {
-            name: {
-                type: DataTypes.STRING,
-            },
-            service_id: {
-                type: DataTypes.INTEGER,
-            },
-            provider_category_id: {
-                type: DataTypes.INTEGER,
-            },
-          affiliated_service_id: {
-            type: DataTypes.INTEGER,
-          },
-            inclusions: {
-                type: DataTypes.JSONB,
-            },
-            exclusion: {
-                type: DataTypes.JSONB,
-            },
-            price_options: {
-                type: DataTypes.JSONB,
-            },
-            ratings: {
-                type: DataTypes.FLOAT,
-            },
-            updated_by: {
-                type: DataTypes.INTEGER,
-                defaultValue: 1
-            },
-            status_type: {
-                type: DataTypes.INTEGER,
-                defaultValue: 1
-            },
-            created_by: {
-                type: DataTypes.INTEGER,
-                defaultValue: 1
-            },
-            created_at: {
-                type: DataTypes.DATE,
-                defaultValue: sequelize.literal('NOW()'),
-            },
-            updated_at: {
-                type: DataTypes.DATE,
-                defaultValue: sequelize.literal('NOW()'),
-            },
-
+  const table_provider_services_mapping = sequelize.define(
+      'table_provider_services_mapping', {
+        name: {
+          type: DataTypes.STRING,
         },
+        service_id: {
+          type: DataTypes.INTEGER,
+        },
+        provider_category_id: {
+          type: DataTypes.INTEGER,
+        },
+        affiliated_service_id: {
+          type: DataTypes.INTEGER,
+        },
+        inclusions: {
+          type: DataTypes.JSONB,
+        },
+        exclusion: {
+          type: DataTypes.JSONB,
+        },
+        price_options: {
+          type: DataTypes.JSONB,
+        },
+        ratings: {
+          type: DataTypes.FLOAT,
+        },
+        updated_by: {
+          type: DataTypes.INTEGER,
+          defaultValue: 1,
+        },
+        status_type: {
+          type: DataTypes.INTEGER,
+          defaultValue: 1,
+        },
+        created_by: {
+          type: DataTypes.INTEGER,
+          defaultValue: 1,
+        },
+        created_at: {
+          type: DataTypes.DATE,
+          defaultValue: sequelize.literal('NOW()'),
+        },
+        updated_at: {
+          type: DataTypes.DATE,
+          defaultValue: sequelize.literal('NOW()'),
+        },
+
+      },
+      {
+        freezeTableName: true,
+        defaultPrimaryKey: true,
+        timestamps: true,
+        underscored: true,
+        tableName: 'table_provider_services_mapping',
+      });
+
+  table_provider_services_mapping.associate = (models) => {
+
+    table_provider_services_mapping.belongsTo(models.table_affiliated_services,
         {
-            freezeTableName: true,
-            defaultPrimaryKey: true,
-            timestamps: true,
-            underscored: true,
-            tableName: 'table_provider_services_mapping',
+          foreignKey: 'service_id',
+          onDelete: 'cascade',
+          onUpdate: 'cascade',
         });
 
-    table_provider_services_mapping.associate = (models) => {
-
-        table_provider_services_mapping.belongsTo(models.table_affiliated_services, {
-            foreignKey: 'service_id',
-            onDelete: 'cascade',
-            onUpdate: 'cascade'
+    table_provider_services_mapping.belongsTo(models.table_provider_categories,
+        {
+          foreignKey: 'provider_category_id',
+          onDelete: 'cascade',
+          onUpdate: 'cascade',
         });
 
-        table_provider_services_mapping.belongsTo(models.table_provider_categories, {
-            foreignKey: 'provider_category_id',
-            onDelete: 'cascade',
-            onUpdate: 'cascade'
+    table_provider_services_mapping.belongsTo(models.users,
+        {foreignKey: 'updated_by', onDelete: 'cascade', onUpdate: 'cascade'});
+    table_provider_services_mapping.belongsTo(models.users,
+        {foreignKey: 'created_by', onDelete: 'cascade', onUpdate: 'cascade'});
+    table_provider_services_mapping.belongsTo(models.statuses,
+        {
+          foreignKey: 'status_type',
+          targetKey: 'status_type',
+          onDelete: 'cascade',
+          onUpdate: 'cascade',
         });
-
-        table_provider_services_mapping.belongsTo(models.users,
-            {foreignKey: 'updated_by', onDelete: 'cascade', onUpdate: 'cascade'});
-        table_provider_services_mapping.belongsTo(models.users,
-            {foreignKey: 'created_by', onDelete: 'cascade', onUpdate: 'cascade'});
-        table_provider_services_mapping.belongsTo(models.statuses,
-            {foreignKey: 'status_type', targetKey: 'status_type', onDelete: 'cascade', onUpdate: 'cascade'});
-    };
-    return table_provider_services_mapping;
+  };
+  return table_provider_services_mapping;
 };
