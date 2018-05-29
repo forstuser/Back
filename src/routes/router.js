eb790eafed8e26c8f61d64ae1d1447b1d65ffc06
varunBB <varun@binbill.com>
2018-05-29 15:54:00 +0530

    appupdate api added

--- src/routes/router.js
+++ src/routes/router.js
@@ -1549,6 +1549,17 @@ function prepareInsightRoutes(insightController, insightRoutes) {
 
 function prepareGeneralRoutes(generalController, generalRoutes) {
   if (generalController) {
+
+    generalRoutes.push({
+      method : 'GET',
+      path : '/appupdate',
+        config : {
+          handler : appVersionHelper.checkForAppUpdate,
+          description : 'Get app latest version',
+        }
+    });
+
+
     generalRoutes.push({
       method: 'POST',
       path: '/contact-us',
